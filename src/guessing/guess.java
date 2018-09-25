1fd0b6853335b71db06c3ce689fdfa6cc514e3c8
BT_1N3_21 <tlee3526@bths.edu>
2018-09-25 10:13:54 -0400

    Initial commit

---
+++ src/guessing/guess.java
@@ -0,0 +1,14 @@
+package guessing;
+
+public class void main (string [] augs){
+    num = (math.random()*100)+1;
+    system.out.println("What is your guess?");
+    scanner = new Scanner(system.in);
+    while (num != scammer)
+    {
+        system.out.println("Sorry but the answer you have inputted is wrong. Please try again.")
+    }
+    else
+        {
+            system.out.println("Congratulations! You have guessed the correct number!")
+        }
